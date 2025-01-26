2e4641f53437d4994f2c84ca77df2b9b1617b9b1
krisine <kristine.nyaga@student.moringaschool.com>
2025-01-26 10:25:03 +0300

    route for getting doctor appointments

--- routes/appointment.js
+++ routes/appointment.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { bookAppointment, calculateAvailableSlots, getAppointment, getAppointments, getPatientAppointments } from "../controllers/appointmentController.js";
+import { bookAppointment, calculateAvailableSlots, getAppointment, getAppointments, getPatientAppointments,getDoctorAppointments } from "../controllers/appointmentController.js";
 import { Auth } from "../middleware/auth.js";
 
 const router = express.Router()
 
 router.get('/patient-appointments',Auth,getPatientAppointments)
+router.get('/doctor-appointments',Auth,getDoctorAppointments)
 router.post('/book',Auth, bookAppointment)
 router.post('/available-slots', Auth, calculateAvailableSlots)
 router.get('/', getAppointments)
