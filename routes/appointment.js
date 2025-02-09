0cf83c4a289cac144b8b4a57ac37138f7c4fcc6f
krisine <kristine.nyaga@student.moringaschool.com>
2025-02-09 23:28:25 +0300

    add update appointment

--- routes/appointment.js
+++ routes/appointment.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { bookAppointment, calculateAvailableSlots, getAppointment, getAppointments, getPatientAppointments,getDoctorAppointments } from "../controllers/appointmentController.js";
+import { bookAppointment, calculateAvailableSlots, getAppointment, getAppointments, getPatientAppointments,getDoctorAppointments, updateAppointment } from "../controllers/appointmentController.js";
 import { Auth } from "../middleware/auth.js";
 
 const router = express.Router()
@@ -10,5 +10,7 @@ router.post('/book',Auth, bookAppointment)
 router.post('/available-slots', Auth, calculateAvailableSlots)
 router.get('/', getAppointments)
 router.get('/:id', Auth, getAppointment)
+router.patch("/:id", Auth, updateAppointment);
+
 
 export default router
