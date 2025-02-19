4f1ab23e8b8079f7d2f3a756ec8607418c937ab0
krisine <kristine.nyaga@student.moringaschool.com>
2025-02-19 21:41:51 +0300

    added queue_number

--- models/Appointment.js
+++ models/Appointment.js
@@ -1,53 +1,62 @@
 import { DataTypes } from 'sequelize';
 import { sequelize } from '../config/db.js';
 
-const Appointment = sequelize.define('Appointment', {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-  patientId: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    references: {
-      model: 'Patients', 
-      key: 'id',
+const Appointment = sequelize.define(
+  "Appointment",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    patientId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "Patients",
+        key: "id",
+      },
+    },
+    doctorId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "Doctors",
+        key: "id",
+      },
+    },
+    date: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+    },
+    startTime: {
+      type: DataTypes.TIME,
+      allowNull: false,
+    },
+    endTime: {
+      type: DataTypes.TIME,
+      allowNull: false,
+    },
+    appointmentDuration: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    status: {
+      type: DataTypes.STRING,
+      defaultValue: "pending",
+      validate: {
+        isIn: [["pending", "completed", "cancelled", "missed"]],
+      },
+    },
+    queueNumber: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 1,
     },
   },
-  doctorId: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    references: {
-      model: 'Doctors', 
-      key: 'id',
-    },
-  },
-  date: {
-    type: DataTypes.DATEONLY,
-    allowNull: false,
-  },
-  startTime: {
-    type: DataTypes.TIME,
-    allowNull: false,
-  },
-  endTime: {
-    type: DataTypes.TIME,
-    allowNull: false,
-  },
-  appointmentDuration: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  status: {
-    type: DataTypes.STRING,
-    defaultValue: 'pending', 
-    validate: {
-      isIn: [['pending', 'completed', 'cancelled']],
-    },
-  },
-}, {
-  timestamps: true 
-});
+  {
+    timestamps: true,
+  }
+);
 
 export default Appointment;
